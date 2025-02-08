cc65d10f81e37e0073b515c9f3569455d4b7e75f
tunguyenarea <tunguyenarea@gmail.com>
2025-02-08 19:55:23 +0700

    Commit 3

--- app/ui/bar-nav.tsx
+++ app/ui/bar-nav.tsx
@@ -22,13 +22,13 @@ export default function BarNav() {
   <>
 
   <nav>
-    <button onClick={toggleBarNav} className="fixed right-0 top-6 z-10 m-3 text-xl bg-white dark:bg-black p-1 rounded opacity-50 text-black dark:text-white p-2">
+    <button onClick={toggleBarNav} className="fixed right-0 top-6 z-30 m-3 text-xl bg-white dark:bg-black p-1 rounded opacity-50 text-black dark:text-white p-2">
       {isOpen ? '✖' : '☰'}
     </button>
 
     {isOpen && (
     <div>
-      <div className="grid md:flex justify-center fixed top-8 bg-white dark:bg-black w-full h-full md:h-18 overflow-auto">
+      <div className="grid md:flex justify-center fixed top-8 z-20 bg-white dark:bg-black w-full h-full md:h-18 overflow-auto">
       {links.map((link) => {
         return (
           <Link key={link.name} href={link.href} className="bg-black text-white dark:bg-white dark:text-black text-center hover:text-white hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 rounded-lg m-3 p-3 w-28 h-12">

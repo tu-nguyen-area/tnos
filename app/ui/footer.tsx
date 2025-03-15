bd6ad75166f54a6bd5117e3f10e25a31c8409bd6
tunguyenarea <tunguyenarea@gmail.com>
2025-03-15 21:46:24 +0700

    Commit 3

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
   <>
 
   <footer className="flex justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white p-6">
     <section>
-      <p>tudellg33579@gmail.com</p>
-      <p>Privacy Policy | Terms of Use</p>
+      <p>Privacy Polity | Terms of Use</p>
+      <p>A product from <Link href="https://tnadvancement.pages.dev/" target="_blank">TN ADVANCEMENT</Link></p>
       <p>&#169; 2025 TN OS Project. All rights reserved.</p>
     </section>
   </footer>

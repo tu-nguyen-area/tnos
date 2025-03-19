f46f1b87e221a3eabd11ca9cdda9102c83893d6b
tu-nguyen-area <161775180+tu-nguyen-area@users.noreply.github.com>
2025-03-19 16:42:33 +0800

    Update footer.tsx

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   <footer className="flex justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white p-6">
     <section>
       <p>Privacy Polity | Terms of Use</p>
-      <p>A product from <Link href="https://tnadvancement.pages.dev/" target="_blank">TN ADVANCEMENT</Link></p>
+      <p>A product from <Link href="https://tu-nguyen.org/" target="_blank">TN ADVANCEMENT</Link></p>
       <p>&#169; 2025 TN OS Project. All rights reserved.</p>
     </section>
   </footer>
